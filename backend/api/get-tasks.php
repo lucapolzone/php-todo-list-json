9697da69111b260480cbdda06a90a188ace41736
lucapolzone <lucapolzone@gmail.com>
2024-03-01 15:58:14 +0100

    fix: v-for now working

--- backend/api/get-tasks.php
+++ backend/api/get-tasks.php
@@ -1,14 +1,19 @@
 <?php
 
 
-$tasks_path = __DIR__ . '../data/tasks.json';
+// $tasks_path = __DIR__ . '/../data/tasks.json';
+
+$tasks_path = '../data/tasks.json';
+
 
 $to_do_list = file_get_contents($tasks_path);
 
-$to_do_list_encoded = json_encode($to_do_list);
+// $to_do_list_encoded = json_encode($to_do_list);
 
 header('Content-Type: application/json');
 
-echo $to_do_list_encoded;
+// echo $to_do_list_encoded; //se faccio l'encode non funziona
+
+echo $to_do_list;
 
 ?>
